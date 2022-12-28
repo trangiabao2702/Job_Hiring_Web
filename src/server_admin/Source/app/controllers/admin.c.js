3130b1b61be1d7976303f0f52675b05350e72991
20120434 <meikaruku@gmail.com>
2022-12-28 23:23:47 +0700

    add manage report and view list reports

--- src/server_admin/Source/app/controllers/admin.c.js
+++ src/server_admin/Source/app/controllers/admin.c.js
@@ -1,233 +1,235 @@
 const adminModel = require("../models/admin.m");
 
 class Admin {
-
-    // [GET] /sign_in
-    async homepage(req, res, next) {
-        try {
-            if (req.isAuthenticated()) {
-                var numberAccount=await adminModel.getNumberAccount();
-                res.render("content_admin/manage_account", {
-                    layout: "main_admin_login",
-                    numberAccount
-                });
-            } else {
-                res.redirect('/auth/sign_in');
-            }
-
-        } catch (error) {
-            next(error);
-        }
-
-    }
-    account_authentication(req, res, next) {
-
-        try {
-            if (req.isAuthenticated()) {
-
-                res.render("content_admin/account_authentication", {
-                    layout: "main_admin_not_login",
-                });
-            } else {
-                res.redirect('/auth/sign_in');
-            }
-
-        } catch (error) {
-            next(error);
-        }
-
-    }
-    async manage_news(req, res, next) {
-        var numberNews=await adminModel.getNumberNews();
-        res.render("content_admin/manage_news", {
-            layout: "main_admin_login",
-            numberNews
+  // [GET] /sign_in
+  async homepage(req, res, next) {
+    try {
+      if (req.isAuthenticated()) {
+        var numberAccount = await adminModel.getNumberAccount();
+        res.render("content_admin/manage_account", {
+          layout: "main_admin_login",
+          numberAccount,
         });
+      } else {
+        res.redirect("/auth/sign_in");
+      }
+    } catch (error) {
+      next(error);
     }
-    async list_news_approved(req, res, next) {
-        var list=await adminModel.getListNew("approved");
-        var length=list.length;
-
-        res.render("content_admin/list_news", {
-            layout: "main_admin_login",
-            list,
-            length
+  }
+  account_authentication(req, res, next) {
+    try {
+      if (req.isAuthenticated()) {
+        res.render("content_admin/account_authentication", {
+          layout: "main_admin_not_login",
         });
+      } else {
+        res.redirect("/auth/sign_in");
+      }
+    } catch (error) {
+      next(error);
     }
-    async list_news_pending(req, res, next) {
-        var list=await adminModel.getListNew("pending");
-        var length=list.length;
+  }
+  async manage_news(req, res, next) {
+    var numberNews = await adminModel.getNumberNews();
+    res.render("content_admin/manage_news", {
+      layout: "main_admin_login",
+      numberNews,
+    });
+  }
+  async list_news_approved(req, res, next) {
+    var list = await adminModel.getListNew("approved");
+    var length = list.length;
 
-        res.render("content_admin/list_news", {
-            layout: "main_admin_login",
-            list,
-            length
-        });
-    }
-    async list_news_locked(req, res, next) {
-        var list=await adminModel.getListNew("locked");
-        var length=list.length;
+    res.render("content_admin/list_news", {
+      layout: "main_admin_login",
+      list,
+      length,
+    });
+  }
+  async list_news_pending(req, res, next) {
+    var list = await adminModel.getListNew("pending");
+    var length = list.length;
 
-        res.render("content_admin/list_news", {
-            layout: "main_admin_login",
-            list,
-            length
-        });
-    }
-    async list_news_removed(req, res, next) {
-        var list=await adminModel.getListNew("deleted");
-        var length=list.length;
-        res.render("content_admin/list_news", {
-            layout: "main_admin_login",
-            list,
-            length
-        });
-    }
-    async detail_news(req, res, next) {
-        var id=req.query.id;
-        var news=await adminModel.detail_news(id);
-        var isApproved=false,isPending=false,isLocked=false,isDeleted=false;
-        if(news.status=="approved")
-        {
-            isApproved=true;
-        }
-        else if(news.status=="locked"){
-            isLocked=true;
-        }
-        else if(news.status=="pending"){
-            isPending=true;
-        }
-        else{
-            isDeleted=true;
-        }
-        var data={isApproved,isPending,isLocked,isDeleted}
-        res.render("content_admin/detail_news", {
-            isDetailNews: true,
-            layout: "main_admin_login",
-            news,
-            data
+    res.render("content_admin/list_news", {
+      layout: "main_admin_login",
+      list,
+      length,
+    });
+  }
+  async list_news_locked(req, res, next) {
+    var list = await adminModel.getListNew("locked");
+    var length = list.length;
 
-        });
+    res.render("content_admin/list_news", {
+      layout: "main_admin_login",
+      list,
+      length,
+    });
+  }
+  async list_news_removed(req, res, next) {
+    var list = await adminModel.getListNew("deleted");
+    var length = list.length;
+    res.render("content_admin/list_news", {
+      layout: "main_admin_login",
+      list,
+      length,
+    });
+  }
+  async detail_news(req, res, next) {
+    var id = req.query.id;
+    var news = await adminModel.detail_news(id);
+    var isApproved = false,
+      isPending = false,
+      isLocked = false,
+      isDeleted = false;
+    if (news.status == "approved") {
+      isApproved = true;
+    } else if (news.status == "locked") {
+      isLocked = true;
+    } else if (news.status == "pending") {
+      isPending = true;
+    } else {
+      isDeleted = true;
     }
-    async list_account_appvoved(req, res, next) {
-        var list=await adminModel.getListAccount("approved");
-        res.render("content_admin/list_account", {
-            layout: "main_admin_login",
-            list
-        });
+    var data = { isApproved, isPending, isLocked, isDeleted };
+    res.render("content_admin/detail_news", {
+      isDetailNews: true,
+      layout: "main_admin_login",
+      news,
+      data,
+    });
+  }
+  async list_account_appvoved(req, res, next) {
+    var list = await adminModel.getListAccount("approved");
+    res.render("content_admin/list_account", {
+      layout: "main_admin_login",
+      list,
+    });
+  }
+  async list_account_pending(req, res, next) {
+    var list = await adminModel.getListAccount("pending");
+    res.render("content_admin/list_account", {
+      layout: "main_admin_login",
+      list,
+    });
+  }
+  async list_account_locked(req, res, next) {
+    var list = await adminModel.getListAccount("locked");
+    res.render("content_admin/list_account", {
+      layout: "main_admin_login",
+      list,
+    });
+  }
+  async detail_account(req, res, next) {
+    var id = req.query.id;
+    var type = req.query.type;
+    var account = await adminModel.getAccountByID(id, type);
+    var isApproved = false,
+      isPending = false,
+      isLocked = false;
+    if (account.status == "approved") {
+      isApproved = true;
+    } else if (account.status == "locked") {
+      isLocked = true;
+    } else {
+      isPending = true;
     }
-    async list_account_pending(req, res, next) {
-        var list=await adminModel.getListAccount("pending");
-        res.render("content_admin/list_account", {
-            layout: "main_admin_login",
-            list
-        });
+    var data = { isApproved, isPending, isLocked };
+    if (type == "employer") {
+      res.render("content_admin/detail_account", {
+        layout: "main_admin_login",
+        account,
+        isEmployer: true,
+        data,
+      });
+    } else {
+      res.render("content_admin/detail_account", {
+        layout: "main_admin_login",
+        account,
+        data,
+      });
     }
-    async list_account_locked(req, res, next) {
-        var list=await adminModel.getListAccount("locked");
-        res.render("content_admin/list_account", {
-            layout: "main_admin_login",
-            list
-        });
-    }
-    async detail_account(req, res, next) {
-        var id=req.query.id;
-        var type=req.query.type;
-        var account=await adminModel.getAccountByID(id,type);
-        var isApproved=false,isPending=false,isLocked=false;
-        if(account.status=="approved")
-        {
-            isApproved=true;
-        }
-        else if(account.status=="locked"){
-            isLocked=true;
-        }
-        else{
-            isPending=true;
-        }
-        var data={isApproved,isPending,isLocked}
-        if(type=="employer")
-        {
-            res.render("content_admin/detail_account", {
-                layout: "main_admin_login",
-                account,
-                isEmployer:true,
-                data
-            });
-        }
-        else{
-            res.render("content_admin/detail_account", {
-                layout: "main_admin_login",
-                account,
-                data
-            });
-        }
-   
-    }
-    async approve_account(req,res,next){
-        var id=req.body.id;
-        var type=req.body.type;
-        await adminModel.status_account(id,"approved",type);
-        res.redirect('back');
-    }
-    async lock_account(req,res,next){
-        var id=req.body.id;
-        var type=req.body.type;
+  }
+  async approve_account(req, res, next) {
+    var id = req.body.id;
+    var type = req.body.type;
+    await adminModel.status_account(id, "approved", type);
+    res.redirect("back");
+  }
+  async lock_account(req, res, next) {
+    var id = req.body.id;
+    var type = req.body.type;
 
-        await adminModel.status_account(id,"locked",type);
-        res.redirect('back');
+    await adminModel.status_account(id, "locked", type);
+    res.redirect("back");
+  }
+  async unlock_account(req, res, next) {
+    var id = req.body.id;
+    var type = req.body.type;
+    await adminModel.status_account(id, "approved", type);
+    res.redirect("back");
+  }
+  async delete_account(req, res, next) {
+    var id = req.body.id;
+    var type = req.body.type;
+    await adminModel.delete_account(id, type);
+    res.redirect("/admin/manage_account");
+  }
+  async status_recruitment(req, res, next) {
+    var id = req.body.id;
+    var type = req.body.type;
+    if (type == "deleted") {
+      await adminModel.status_recruitment(id, type);
+      return res.redirect("/admin/manage_news");
+    } else if (type == "locked") {
+      await adminModel.status_recruitment(id, type);
+      return res.redirect("back");
+    } else if (type == "unlock") {
+      type = "approved";
+      await adminModel.status_recruitment(id, type);
+      return res.redirect("back");
+    } else {
+      await adminModel.status_recruitment(id, type);
+      return res.redirect("back");
     }
-    async unlock_account(req,res,next){
-        var id=req.body.id;
-        var type=req.body.type;
-        await adminModel.status_account(id,"approved",type);
-        res.redirect('back');
+  }
+  // async list_applied(req, res, next) {
+  //     var id=req.query.id;
+  //     var list=await adminModel.getListApplied(id);
+  //     res.render("content_admin/list_account", {
+  //         layout: "main_admin_login",
+  //         list
+  //     });
+  // }
+  // [GET] /forgot_pw
+
+  async manage_reports(req, res, next) {
+    const _manage_reports = await adminModel.getAllReports();
+
+    res.render("content_admin/manage_reports", {
+      layout: "main_admin_login",
+      data: {
+        manage_reports: JSON.stringify(_manage_reports),
+      },
+    });
+  }
+
+  list_reports(req, res, next) {
+    let _list_reports = "";
+    if (req.body.list_pending_reports == "") {
+      _list_reports = req.body.list_approved_reports;
+    } else {
+      _list_reports = req.body.list_pending_reports;
     }
-    async delete_account(req,res,next){
-        var id=req.body.id;
-        var type=req.body.type;
-        await adminModel.delete_account(id,type);
-        res.redirect('/admin/manage_account');
-    }
-    async status_recruitment(req,res,next){
-        var id=req.body.id;
-        var type=req.body.type;
-        if(type=="deleted")
-        {
-            await adminModel.status_recruitment(id,type);
-           return res.redirect('/admin/manage_news');
-        }
-        else if(type=="locked")
-        {
-            await adminModel.status_recruitment(id,type);
-            return res.redirect('back');
-
-
-        }
-        else if(type=="unlock")
-        {
-            type="approved";
-            await adminModel.status_recruitment(id,type);
-            return res.redirect('back');
-
-
-        }
-        else{
-            await adminModel.status_recruitment(id,type);
-           return res.redirect('back');
-
-        }
-    }
-    // async list_applied(req, res, next) {
-    //     var id=req.query.id;
-    //     var list=await adminModel.getListApplied(id);
-    //     res.render("content_admin/list_account", {
-    //         layout: "main_admin_login",
-    //         list
-    //     });
-    // }
-    // [GET] /forgot_pw
-
+    console.log(_list_reports);
+    res.render("content_admin/list_reports", {
+      layout: "main_admin_login",
+      data: {
+        list_reports: _list_reports,
+      },
+    });
+  }
 }
 
 module.exports = new Admin();
--- src/server_admin/Source/app/models/admin.m.js
+++ src/server_admin/Source/app/models/admin.m.js
@@ -1,211 +1,199 @@
-
-const e = require('express');
-const db = require('../../config/db/index'); // getDatabase
-const storage = require('../../config/db/storage'); // getStorage
+const e = require("express");
+const db = require("../../config/db/index"); // getDatabase
+const storage = require("../../config/db/storage"); // getStorage
 
 module.exports = {
+  getAmdinbyUsername: async (email) => {
+    const adminCollection = db.collection("admins");
+    const snapshot = await adminCollection.where("email", "==", email).get();
 
-    getAmdinbyUsername: async (email) => {
-        const adminCollection = db.collection('admins');
-        const snapshot = await adminCollection.where('email', '==', email).get();
+    var user = null;
+    snapshot.forEach((doc) => {
+      user = doc.data();
+    });
 
-        var user = null;
-        snapshot.forEach(doc => {
-            user = doc.data();
-        });
+    return user;
+  },
+  getNumberAccount: async () => {
+    const candidateCollection = db.collection("candidates");
+    const emplpyerCollection = db.collection("employers");
 
-        return user;
-    },
-    getNumberAccount: async() =>{
-        const candidateCollection = db.collection('candidates');
-        const emplpyerCollection=db.collection('employers')
+    const candidate = await candidateCollection.get();
+    const employer = await emplpyerCollection.get();
 
-        const candidate = await candidateCollection.get();
-        const employer = await emplpyerCollection.get();
+    var numberApproved = 0;
+    var numberPending = 0;
+    var numberlocked = 0;
 
-        var numberApproved=0
-        var numberPending=0;
-        var numberlocked=0;
+    candidate.forEach((doc) => {
+      if (doc.data().status == "approved") {
+        numberApproved += 1;
+      } else if (doc.data().status == "locked") {
+        numberlocked += 1;
+      }
+    });
+    employer.forEach((doc) => {
+      if (doc.data().status == "approved") {
+        numberApproved += 1;
+      } else if (doc.data().status == "pending") {
+        numberPending += 1;
+      } else {
+        numberlocked += 1;
+      }
+    });
+    var data = { numberApproved, numberPending, numberlocked };
+    return data;
+  },
+  getNumberNews: async () => {
+    const recruitmentsCollection = db.collection("recruitments");
 
-        candidate.forEach(doc => {
-            if(doc.data().status=="approved"){
-                numberApproved+=1;
-            }
-            else if(doc.data().status=="locked")
-            {
-                numberlocked+=1;
-            }
-        });
-        employer.forEach(doc=>{
-            if(doc.data().status=="approved"){
-                numberApproved+=1;
-            }
-            else if(doc.data().status=="pending"){
-                numberPending+=1;
-            }else{
-                numberlocked+=1;
-            }
-        })
-        var data={numberApproved,numberPending,numberlocked};
-        return data;
-    },
-    getNumberNews: async()=>{
-        const recruitmentsCollection = db.collection('recruitments');
+    const recruitments = await recruitmentsCollection.get();
 
-        const recruitments = await recruitmentsCollection.get();
+    var numberApproved = 0;
+    var numberPending = 0;
+    var numberLocked = 0;
+    var numberRemove = 0;
 
-        var numberApproved=0
-        var numberPending=0;
-        var numberLocked=0;
-        var numberRemove=0;
+    recruitments.forEach((doc) => {
+      if (doc.data().status == "approved") {
+        numberApproved += 1;
+      } else if (doc.data().status == "pending") {
+        numberPending += 1;
+      } else if (doc.data().status == "locked") {
+        numberLocked += 1;
+      } else {
+        numberRemove += 1;
+      }
+    });
 
-        recruitments.forEach(doc => {
-            if(doc.data().status=="approved"){
-                numberApproved+=1;
-            }
-            else if(doc.data().status=="pending")
-            {
-                numberPending+=1;
-            }
-            else if(doc.data().status=="locked")
-            {
-                numberLocked+=1;
-            }
-            else{
-                numberRemove+=1;
-            }
-        });
-       
-        var data={numberApproved,numberPending,numberLocked,numberRemove};
-        return data;
-    },
-    getListAccount: async status =>{
-        
-        const candidateCollection = db.collection('candidates');
-        const employerCollection = db.collection('employers');
+    var data = { numberApproved, numberPending, numberLocked, numberRemove };
+    return data;
+  },
+  getListAccount: async (status) => {
+    const candidateCollection = db.collection("candidates");
+    const employerCollection = db.collection("employers");
 
-        const candidate = await candidateCollection.where('status', '==', status).get();
-        const employer=await employerCollection.where('status', '==', status).get();
+    const candidate = await candidateCollection.where("status", "==", status).get();
+    const employer = await employerCollection.where("status", "==", status).get();
 
-        var list=[];
-        var user=null;
-        var stt=0;
-        candidate.forEach(doc => {
-            user = doc.data();
-            stt++;
-            user.typeAccount="Ứng viên tìm việc";
-            user.type="candidate";
-            user.creation_date = new Date(user.creation_date.toDate().toDateString()).toLocaleString('VN');
-            user.stt=stt;
-            user.doc=doc.id;
-            list.push(user);
-        });
-        employer.forEach(doc => {
-            user = doc.data();
-            stt++;
-            user.typeAccount="Nhà tuyển dụng";
-            user.type="employer";
-            user.stt=stt;
-            user.doc=doc.id;
-            list.push(user);
-        });
+    var list = [];
+    var user = null;
+    var stt = 0;
+    candidate.forEach((doc) => {
+      user = doc.data();
+      stt++;
+      user.typeAccount = "Ứng viên tìm việc";
+      user.type = "candidate";
+      user.creation_date = new Date(user.creation_date.toDate().toDateString()).toLocaleString("VN");
+      user.stt = stt;
+      user.doc = doc.id;
+      list.push(user);
+    });
+    employer.forEach((doc) => {
+      user = doc.data();
+      stt++;
+      user.typeAccount = "Nhà tuyển dụng";
+      user.type = "employer";
+      user.stt = stt;
+      user.doc = doc.id;
+      list.push(user);
+    });
 
-        return list;
-    },
-    getListNew: async status =>{
-        
-        const recruitmentCollection = db.collection('recruitments');
-        const employerCollection = db.collection('employers');
-        const cur_vit = db.collection('curriculum_vitaes');
+    return list;
+  },
+  getListNew: async (status) => {
+    const recruitmentCollection = db.collection("recruitments");
+    const employerCollection = db.collection("employers");
+    const cur_vit = db.collection("curriculum_vitaes");
 
+    const recruitment = await recruitmentCollection.where("status", "==", status).get();
 
-        const recruitment = await recruitmentCollection.where('status', '==', status).get();
+    var list = [];
+    var recruit = null;
+    var stt = 0;
+    recruitment.forEach((doc) => {
+      recruit = doc.data();
+      stt++;
+      recruit.creation_date = new Date(recruit.creation_date.toDate().toDateString()).toLocaleString("VN");
+      recruit.stt = stt;
+      recruit.doc = doc.id;
+      list.push(recruit);
+    });
 
-        var list=[];
-        var recruit=null;
-        var stt=0;
-        recruitment.forEach(doc => {
-            recruit = doc.data();
-            stt++;
-            recruit.creation_date = new Date(recruit.creation_date.toDate().toDateString()).toLocaleString('VN');
-            recruit.stt=stt;
-            recruit.doc=doc.id;
-            list.push(recruit);
-        });
-
-        for(let i=0;i<list.length;i++)
-        {
-            const employer=await employerCollection.doc(list[i].belong_employer).get();
-            list[i].nameEmployer=employer.data().name;
-            const numberCandidate=await cur_vit.where('id_recruitment','==',list[i].doc);
-            const snapshot = await numberCandidate.count().get();
-            list[i].numberCV=snapshot.data().count;
-        }
-        
-
-        return list;
-    },
-    getAccountByID: async (id,type) =>{
-        const accountCollection = db.collection(type+"s");
-        const docSnap = await accountCollection.doc(id).get();
-        var state="";
-        if(docSnap.data().status=="approved")
-        {
-            state="Đã duyệt";
-        }
-        else if(docSnap.data().status=="pending"){
-            state="Chờ duyệt";
-
-        }
-        else{
-            state="Đã khóa";
-
-        }
-        var user=docSnap.data();
-        user.creation_date = new Date(user.creation_date.toDate().toDateString()).toLocaleString('VN');
-        var account = { id,type,state, ...user};
-        return account;
-    },
-    detail_news: async (id) =>{
-        const recruitmentCollection = db.collection("recruitments");
-        const employerCollection = db.collection("employers");
-
-        const recruitment = await recruitmentCollection.doc(id).get();
-        var rect=recruitment.data();
-        var state="";
-        if(rect.status=="approved")
-        {
-            state="Đã duyệt";
-        }
-        else if(rect.status=="pending"){
-            state="Chờ duyệt";
-
-        }
-        else if(rect.status=="locked"){
-            state="Đã khóa"
-
-        }
-        else
-        {
-            state="Đã xóa";
-        }
-        const employer = await employerCollection.doc(rect.belong_employer).get();
-        rect.nameEmployer=employer.data().name;
-        rect.address=employer.data().district+"," +employer.data().province;
-        rect.avatar=employer.data().avatar;
-        rect.creation_date = new Date(rect.creation_date.toDate().toDateString()).toLocaleString('VN');
-        rect.due_date = new Date(rect.due_date.toDate().toDateString()).toLocaleString('VN');
-        var recruit = { id,state,...rect};
-        return recruit;
-    },
-    status_account: async (id,state,type) =>{
-        db.collection( type+"s").doc(id).update({status: state});
-    },
-    delete_account: async (id,type) =>{
-        db.collection( type+"s").doc(id).delete();
-    },
-    status_recruitment:async(id,type) =>{
-        db.collection("recruitments").doc(id).update({status: type});
+    for (let i = 0; i < list.length; i++) {
+      const employer = await employerCollection.doc(list[i].belong_employer).get();
+      list[i].nameEmployer = employer.data().name;
+      const numberCandidate = await cur_vit.where("id_recruitment", "==", list[i].doc);
+      const snapshot = await numberCandidate.count().get();
+      list[i].numberCV = snapshot.data().count;
     }
 
-}
+    return list;
+  },
+  getAccountByID: async (id, type) => {
+    const accountCollection = db.collection(type + "s");
+    const docSnap = await accountCollection.doc(id).get();
+    var state = "";
+    if (docSnap.data().status == "approved") {
+      state = "Đã duyệt";
+    } else if (docSnap.data().status == "pending") {
+      state = "Chờ duyệt";
+    } else {
+      state = "Đã khóa";
+    }
+    var user = docSnap.data();
+    user.creation_date = new Date(user.creation_date.toDate().toDateString()).toLocaleString("VN");
+    var account = { id, type, state, ...user };
+    return account;
+  },
+  detail_news: async (id) => {
+    const recruitmentCollection = db.collection("recruitments");
+    const employerCollection = db.collection("employers");
+
+    const recruitment = await recruitmentCollection.doc(id).get();
+    var rect = recruitment.data();
+    var state = "";
+    if (rect.status == "approved") {
+      state = "Đã duyệt";
+    } else if (rect.status == "pending") {
+      state = "Chờ duyệt";
+    } else if (rect.status == "locked") {
+      state = "Đã khóa";
+    } else {
+      state = "Đã xóa";
+    }
+    const employer = await employerCollection.doc(rect.belong_employer).get();
+    rect.nameEmployer = employer.data().name;
+    rect.address = employer.data().district + "," + employer.data().province;
+    rect.avatar = employer.data().avatar;
+    rect.creation_date = new Date(rect.creation_date.toDate().toDateString()).toLocaleString("VN");
+    rect.due_date = new Date(rect.due_date.toDate().toDateString()).toLocaleString("VN");
+    var recruit = { id, state, ...rect };
+    return recruit;
+  },
+  status_account: async (id, state, type) => {
+    db.collection(type + "s")
+      .doc(id)
+      .update({ status: state });
+  },
+  delete_account: async (id, type) => {
+    db.collection(type + "s")
+      .doc(id)
+      .delete();
+  },
+  status_recruitment: async (id, type) => {
+    db.collection("recruitments").doc(id).update({ status: type });
+  },
+
+  getAllReports: async () => {
+    const rs = await db.collection("reports").get();
+
+    let _list_reports = [];
+    rs.forEach((doc) => {
+      _list_reports.push(doc.data());
+      _list_reports[_list_reports.length - 1].id_report = doc.id;
+    });
+
+    return _list_reports;
+  },
+};
